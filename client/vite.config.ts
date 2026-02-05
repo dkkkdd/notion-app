83edba9693cc6d7c1ad91053dc67717b0024f6a3
Jevgenia <fruffrr@gmail.com>
2026-02-05 16:34:33 +0200

    small fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,6 +15,17 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": {
